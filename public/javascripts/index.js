b659357c4d2a762ee6fe09a14572d2c7dd664624
cihatdev <cihat02020202@gmail.com>
2021-03-26 00:29:18 +0300

    update project new version that new features

---
+++ public/javascripts/index.js
@@ -0,0 +1,38 @@
+const element_ul = document.querySelector(".words_ol");
+const reminder_button = document.querySelector(".reminder_button");
+const translate_word = document.querySelector(".translate_word");
+
+const processLineByLine = () => {
+  fetch("../data/words.txt")
+    .then((response) => response.text())
+    .then((data) => {
+      createLi(data);
+    });
+};
+
+const createLi = (words) => {
+  randomWord(words);
+  let all_words = words.split("\n");
+  for (let i = 0; i < all_words.length; i++) {
+    let new_li = document.createElement("li");
+    new_li.innerHTML = all_words[i];
+    element_ul.appendChild(new_li);
+  }
+};
+
+processLineByLine();
+
+const randomWord = (words) => {
+  let randomNumber;
+  let array = words.split("\n");
+  // console.log(array);
+  let newArr = array.filter((word) => word.split(" = ").length === 2);
+  let border = newArr.length;
+
+  reminder_button.addEventListener("click", (e) => {
+    randomNumber = Math.floor(Math.random() * border + 1);
+    // console.log(randomNumber);
+    translate_word.textContent = array[randomNumber];
+    // translate_word.classList.add(".hide_translate_word");
+  });
+};
