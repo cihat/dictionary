9220c030f1bf18d89c7329e9b42fe1655828450e
Cihat Salik <cihatsalik1@gmail.com>
2022-10-11 23:48:30 +0300

    Add landing page

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,23 @@
-import { createRouter, createWebHistory } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory } from 'vue-router'
+import App from '../views/App.vue'
+import LandingPage from '../views/LandingPage.vue'
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'landing-page',
+    component: LandingPage
   },
-];
+  {
+    path: '/app',
+    name: 'app',
+    component: App
+  }
+]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  routes
+})
 
-export default router;
+export default router
---
+++ src/views/LandingPage.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+  name: "landing-page",
+}
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to Dictionary</h1>
+    <span>
+      <p>
+        This is a dictionary app that allows you to search for words and their
+        definitions. You can also search for words in the Youglish API to see
+        videos of how to pronounce the word.
+      </p>
+      <p>
+        This app was created by <a href="https://github.com/cihat/dictionary" target="_blank">@cihat</a>
+      </p>
+    </span>
+    <button>
+      <router-link to="/app">Go to App</router-link>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
